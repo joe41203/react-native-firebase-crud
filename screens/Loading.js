1b0a901890c410cd08bceffd8eb3fe3537e04ef5
joe <kinjoyuji@gmail.com>
2019-02-03 23:08:43 +0900

    画面をローディング後、ログイン済みであればHome画面、そうでなければSignUp画面を表示させる実装

--- screens/Loading.js
+++ screens/Loading.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { View } from 'native-base';
 import { ActivityIndicator, StyleSheet } from 'react-native';
+// 追加
+import firebase from 'firebase';
 
 export default class Loading extends React.Component {
+  // 追加
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(this.props);
+      this.props.navigation.navigate(user ? 'Home' : 'SignUp');
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
