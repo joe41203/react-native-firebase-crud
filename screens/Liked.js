91fc92c1598538d7d5ff8a6c9d32c70e9c1db33e
joe <kinjoyuji@gmail.com>
2019-02-07 01:06:20 +0900

    liked画面実装(サンプル)

--- screens/Liked.js
+++ screens/Liked.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
 import {
   Container,
   Header,
   Content,
-  Card,
-  CardItem,
+  List,
+  ListItem,
   Thumbnail,
   Text,
-  Button,
-  Icon,
   Left,
   Body,
-  Right
+  Right,
+  Button
 } from 'native-base';
 export default class Liked extends Component {
   render() {
@@ -20,48 +18,34 @@ export default class Liked extends Component {
       <Container>
         <Header />
         <Content>
-          <Card>
-            <CardItem>
-              <Left>
-                <Thumbnail
-                  source={{
-                    uri:
-                      'https://d1f5hsy4d47upe.cloudfront.net/60/60feaa940647ad2eef651a49a6d2147e_t.jpeg'
-                  }}
-                />
-                <Body>
-                  <Text>NativeBase</Text>
-                  <Text note>GeekyAnts</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem cardBody>
-              <Image
-                source={{
-                  uri:
-                    'https://d1f5hsy4d47upe.cloudfront.net/60/60feaa940647ad2eef651a49a6d2147e_t.jpeg'
-                }}
-                style={{ height: 200, width: null, flex: 1 }}
-              />
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent>
-                  <Icon active name='thumbs-up' />
-                  <Text>12 Likes</Text>
-                </Button>
-              </Left>
-              <Body>
-                <Button transparent>
-                  <Icon active name='chatbubbles' />
-                  <Text>4 Comments</Text>
-                </Button>
-              </Body>
-              <Right>
-                <Text>11h ago</Text>
-              </Right>
-            </CardItem>
-          </Card>
+          {[...Array(20)].map((_, i) => {
+            return (
+              <List key={i}>
+                <ListItem thumbnail>
+                  <Left>
+                    <Thumbnail
+                      square
+                      source={{
+                        uri:
+                          'https://d1f5hsy4d47upe.cloudfront.net/60/60feaa940647ad2eef651a49a6d2147e_t.jpeg'
+                      }}
+                    />
+                  </Left>
+                  <Body>
+                    <Text>Sankhadeep</Text>
+                    <Text note numberOfLines={1}>
+                      Its time to build a difference . .
+                    </Text>
+                  </Body>
+                  <Right>
+                    <Button transparent>
+                      <Text>View</Text>
+                    </Button>
+                  </Right>
+                </ListItem>
+              </List>
+            );
+          })}
         </Content>
       </Container>
     );
