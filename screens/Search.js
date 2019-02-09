3eaad80ab013aa7f7f07dd929f8356dba6f668f3
joe <kinjoyuji@gmail.com>
2019-02-09 14:09:07 +0900

    search UI実装

--- screens/Search.js
+++ screens/Search.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
-import {
-  Container,
-  Header,
-  Content,
-  Text,
-  Input,
-  Item,
-  Icon
-} from 'native-base';
+import { Container, Header, Content, Input, Item, Icon } from 'native-base';
+import { Dimensions, Image } from 'react-native';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 
 export default class Search extends Component {
   constructor(props) {
@@ -15,6 +9,20 @@ export default class Search extends Component {
   }
 
   render() {
+    const { width } = Dimensions.get('window');
+    const datas = [
+      { id: 1, image: 'https://bootdey.com/img/Content/avatar/avatar1.png' },
+      { id: 2, image: 'https://bootdey.com/img/Content/avatar/avatar2.png' },
+      { id: 3, image: 'https://bootdey.com/img/Content/avatar/avatar3.png' },
+      { id: 4, image: 'https://bootdey.com/img/Content/avatar/avatar4.png' },
+      { id: 5, image: 'https://bootdey.com/img/Content/avatar/avatar5.png' },
+      { id: 6, image: 'https://bootdey.com/img/Content/avatar/avatar6.png' },
+      { id: 7, image: 'https://bootdey.com/img/Content/avatar/avatar7.png' },
+      { id: 8, image: 'https://bootdey.com/img/Content/avatar/avatar1.png' },
+      { id: 9, image: 'https://bootdey.com/img/Content/avatar/avatar2.png' },
+      { id: 10, image: 'https://bootdey.com/img/Content/avatar/avatar3.png' }
+    ];
+
     return (
       <Container>
         <Header searchBar>
@@ -24,7 +32,56 @@ export default class Search extends Component {
           </Item>
         </Header>
         <Content>
-          <Text>HelloWorld</Text>
+          <Grid>
+            {[...Array(10)].map((_, i) => {
+              return (
+                <Row key={i}>
+                  <Col
+                    style={{
+                      width: width / 3,
+                      margin: 1
+                    }}
+                  >
+                    <Image
+                      style={{ height: width / 3, flex: 1 }}
+                      source={{
+                        uri:
+                          'https://bootdey.com/img/Content/avatar/avatar3.png'
+                      }}
+                    />
+                  </Col>
+                  <Col
+                    style={{
+                      width: width / 3,
+                      margin: 1
+                    }}
+                  >
+                    <Image
+                      style={{ height: width / 3, flex: 1 }}
+                      source={{
+                        uri:
+                          'https://bootdey.com/img/Content/avatar/avatar2.png'
+                      }}
+                    />
+                  </Col>
+                  <Col
+                    style={{
+                      width: width / 3,
+                      margin: 1
+                    }}
+                  >
+                    <Image
+                      style={{ height: width / 3, flex: 1 }}
+                      source={{
+                        uri:
+                          'https://bootdey.com/img/Content/avatar/avatar4.png'
+                      }}
+                    />
+                  </Col>
+                </Row>
+              );
+            })}
+          </Grid>
         </Content>
       </Container>
     );
