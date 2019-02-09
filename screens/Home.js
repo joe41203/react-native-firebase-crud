8e47efc4c9766d7e0ee7917dbea21d11b5c07f08
joe <kinjoyuji@gmail.com>
2019-02-09 14:41:26 +0900

    Refresh Controlの実装

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, Body, Title } from 'native-base';
+import { RefreshControl } from 'react-native';
 import PostedCard from '../containers/PostedCard';
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      refreshing: false
+    };
+  }
+
+  _onRefresh = () => {
+    this.setState({ refreshing: true });
+    setTimeout(() => this.setState({ refreshing: false }), 1000);
+  };
+
   render() {
     return (
       <Container>
@@ -11,6 +25,10 @@ export default class Home extends Component {
           </Body>
         </Header>
         <Content>
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
           {[...Array(5)].map((_, i) => (
             <PostedCard key={i} />
           ))}
